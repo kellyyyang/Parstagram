2589884e0ff95e6bfebfe42f1330df974c639519
kellyyyang <78131256+kellyyyang@users.noreply.github.com>
2022-06-13 16:03:19 -0700

    Users can sign up from the login page.

--- app/src/main/java/com/codepath/parstagram/LoginActivity.java
+++ app/src/main/java/com/codepath/parstagram/LoginActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,6 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     EditText etUsername;
     EditText etPassword;
     Button btnLogin;
+    Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +36,7 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +45,43 @@ public class LoginActivity extends AppCompatActivity {
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
                 loginUser(username, password);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick sign up button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                signUpUser(username, password);
+            }
+        });
+
+
+    }
+
+    private void signUpUser(String username, String password) {
+        Log.i(TAG, "Attempting to sign up user " + username);
+
+        ParseUser user = new ParseUser();
+
+        // Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    goMainActivity();
+                    Log.i(TAG, "Going to main activity.");
+                    Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT);
+                } else {
+                    Log.e(TAG, "Issue with sign up ", e);
+                    Toast.makeText(LoginActivity.this, "Issue with login!", Toast.LENGTH_SHORT);
+                    return;
+                }
             }
         });
     }
--- app/src/main/java/com/codepath/parstagram/MainActivity.java
+++ app/src/main/java/com/codepath/parstagram/MainActivity.java
@@ -2,13 +2,46 @@ package com.codepath.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onLogoutButton(View view) {
+        ParseUser.logOutInBackground(new LogOutCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with login ", e);
+                    Toast.makeText(MainActivity.this, "Issue with logout!", Toast.LENGTH_SHORT);
+                    return;
+                } else {
+                    goLoginActivity();
+                    Log.i(TAG, "Going to login activity.");
+                    Toast.makeText(MainActivity.this, "Success!", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+    }
+
+    private void goLoginActivity() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
     }
 }
