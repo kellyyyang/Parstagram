e65eccc96a814dd1047c26b7d2c737cda8e40f0b
kellyyyang <78131256+kellyyyang@users.noreply.github.com>
2022-06-14 13:33:16 -0700

    Finished Step 4: Querying the Parse database.

--- app/src/main/java/com/codepath/parstagram/MainActivity.java
+++ app/src/main/java/com/codepath/parstagram/MainActivity.java
@@ -6,20 +6,55 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.LogOutCallback;
 import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
 
+    EditText etDescription;
+    Button btnPicture;
+    Button btnPost;
+    ImageView ivPhoto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etDescription = findViewById(R.id.etDescription);
+        btnPicture = findViewById(R.id.btnPicture);
+        btnPost = findViewById(R.id.btnPost);
+        ivPhoto = findViewById(R.id.ivPhoto);
+
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts ", e);
+                    return;
+                } for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", user: " + post.getUser().getUsername());
+                }
+            }
+        });
     }
 
     public void onLogoutButton(View view) {
